1cf84be7e7438079adda2e722102c694e06b5588
dansen <370283593@qq.com>
2014-05-26 16:07:24 +0800

    c

---
+++ src/Frontend/FileUtility.cpp
@@ -0,0 +1,153 @@
+/*
+
+Decoda
+Copyright (C) 2007-2013 Unknown Worlds Entertainment, Inc. 
+
+This file is part of Decoda.
+
+Decoda is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Decoda is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Decoda.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "FileUtility.h"
+
+#include <shlobj.h>
+
+static int _fileSize(const char * path)
+{
+	FILE * file = fopen(path, "rb");
+	if (!file){
+		return 0;
+	}
+
+	fseek(file, 0, SEEK_END);
+	int size = ftell(file);
+	fclose(file);
+
+	return size;
+}
+
+std::string str2ansi(const char * str)
+{
+	int utf8Size = MultiByteToWideChar(CP_UTF8, 0, str, -1, NULL, 0);
+	WCHAR * wszBuf = new WCHAR[utf8Size];
+	memset(wszBuf, 0, sizeof(WCHAR)*utf8Size);
+	MultiByteToWideChar(CP_UTF8, 0, str, -1, wszBuf, utf8Size);
+
+	int ansiSize = WideCharToMultiByte(CP_ACP, 0, wszBuf, -1, NULL, 0, NULL, NULL);
+	char * buf = new char[ansiSize];
+	WideCharToMultiByte(CP_ACP, 0, wszBuf, -1, buf, ansiSize, NULL, NULL);
+	std::string tmp = buf;
+	delete[] buf;
+	return tmp;
+}
+
+bool saveFile(const char * path, std::string & str)
+{
+	FILE * fp = fopen(path, "wb");
+
+	if (str.empty() == false){
+		int utf8Size = MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, NULL, 0);
+		WCHAR * wszBuf = new WCHAR[utf8Size];
+		memset(wszBuf, 0, sizeof(WCHAR)*utf8Size);
+		MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, wszBuf, utf8Size);
+
+		int ansiSize = WideCharToMultiByte(CP_UTF8, 0, wszBuf, -1, NULL, 0, NULL, NULL);
+		char * buf = new char[ansiSize];
+		WideCharToMultiByte(CP_UTF8, 0, wszBuf, -1, buf, ansiSize, NULL, NULL);
+		
+		fwrite(buf, ansiSize-1, 1, fp);
+
+		delete[] buf;
+	}
+
+	fclose(fp);
+
+	return true;
+}
+
+std::string fileStringANSI(const char * path)
+{
+	int size = _fileSize(path);
+	FILE * fp = fopen(path, "rb");
+	char * buf = new char[size+1];
+	fread(buf, 1, size, fp);
+	buf[size] = 0;
+	fclose(fp);
+
+	std::string tmp = str2ansi(buf);
+	delete[] buf;
+
+	return tmp;
+}
+
+void ShowFileInFolder (wxFileName& inPath) {
+  if (!inPath.IsOk())
+    return;
+
+  typedef HRESULT (WINAPI* SHOpenFolderAndSelectItemsPtr)(
+    LPCITEMIDLIST pidlFolder, 
+    UINT cidl, 
+    LPCITEMIDLIST* apidl, 
+    DWORD dwFlags);
+
+  wxString& fullPath = inPath.GetFullPath();  
+  wxString& path = inPath.GetPath();
+
+  static SHOpenFolderAndSelectItemsPtr openFolderFunction = NULL;
+  static bool intializedFunction = true;
+
+  if (intializedFunction) {
+    intializedFunction = false;
+    HMODULE shell32 = GetModuleHandle(wxT("shell32.dll"));
+    if (shell32 == NULL)
+      return;
+
+    openFolderFunction = reinterpret_cast<SHOpenFolderAndSelectItemsPtr>(GetProcAddress(shell32, "SHOpenFolderAndSelectItems"));
+    if (!openFolderFunction) {
+      ShellExecute(NULL, _T("open"), fullPath.c_str(), NULL, NULL, SW_SHOW);
+      return;
+    }
+  }
+
+  IShellFolder* desktopFolder;
+  HRESULT result = SHGetDesktopFolder(&desktopFolder);
+  if(FAILED(result))
+    return;
+
+  LPITEMIDLIST folderPath;
+  LPITEMIDLIST filePath;
+
+  result = desktopFolder->ParseDisplayName(NULL, NULL, (LPOLESTR)(const wchar_t *)path.wc_str(wxConvUTF8), NULL, &folderPath, NULL);
+  if (SUCCEEDED(result)) {  
+    result = desktopFolder->ParseDisplayName(NULL, NULL, (LPOLESTR)(const wchar_t *)fullPath.wc_str(wxConvUTF8), NULL, &filePath, NULL);
+    if (SUCCEEDED(result)) {
+
+      const ITEMIDLIST* highlight[] = {
+        {filePath},
+      };
+
+      (*openFolderFunction)(folderPath, ARRAYSIZE(highlight), highlight, NULL);      
+    }
+  }
+
+  if (folderPath)
+    CoTaskMemFree(folderPath);
+
+  if (filePath)
+    CoTaskMemFree(filePath);
+
+  desktopFolder->Release();
+  
+}
---
+++ src/Frontend/MainApp.cpp
@@ -0,0 +1,254 @@
+/*
+
+Decoda
+Copyright (C) 2007-2013 Unknown Worlds Entertainment, Inc. 
+
+This file is part of Decoda.
+
+Decoda is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Decoda is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Decoda.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "MainApp.h"
+#include "MainFrame.h"
+#include "SingleInstance.h"
+#include "CrashHandler.h"
+#include "Report.h"
+#include "Config.h"
+
+#include <sstream>
+
+#include <wx/cmdline.h>
+
+IMPLEMENT_APP(MainApp)
+
+// Like a version number, but used to track updates. This should be incremented
+// whenever a new build is released.
+const unsigned int  MainApp::s_buildNumber = 1034;
+const char*         MainApp::s_versionDesc = "Version 1.17 (Build 1035) Beta 1";
+
+std::string UrlEncode(const std::string& string)
+{
+    
+    std::string result;
+    result.reserve(string.length());
+
+    for (unsigned int i = 0; i < string.length(); ++i)
+    {
+		if (string[i] != '_' && !isalnum((unsigned char)string[i]))
+        {
+            char buffer[4];
+            sprintf(buffer, "%%%02x", static_cast<int>(static_cast<unsigned char>(string[i])));
+            result += buffer;
+        }
+        else
+        {
+            result += string[i];
+        }
+    }
+
+    return result;
+
+}
+
+/**
+ * Called when the application crashes.
+ */
+void CrashCallback(void* address)
+{
+
+    extern const unsigned int g_buildNumber;
+
+    Report report;
+    report.AttachMiniDump();
+
+    if (report.Preview())
+    {
+
+        std::stringstream stream;
+        stream << "Decoda (Build " << MainApp::s_buildNumber << ")";
+
+        std::string buffer = UrlEncode(stream.str());
+        
+        char serverAddress[1024];
+        sprintf(serverAddress, "http://www.unknownworlds.com/game_scripts/report/crash.php?application=%s&address=0x%08X", buffer.c_str(), reinterpret_cast<unsigned int>(address));
+        report.Submit(serverAddress, NULL);
+    
+    }
+
+}
+
+MainApp::MainApp()
+{
+    CrashHandler::SetCallback( CrashCallback );
+}
+
+void redirectSTD2console()
+{
+	AllocConsole();
+	CreateConsoleScreenBuffer(GENERIC_WRITE | GENERIC_READ, 0, 0, CONSOLE_TEXTMODE_BUFFER, 0);
+
+	HANDLE input = GetStdHandle(STD_INPUT_HANDLE);
+	HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE);
+	HANDLE eoutput = GetStdHandle(STD_ERROR_HANDLE);
+
+	FILE *fi = _fdopen(_open_osfhandle((long)input, _O_TEXT), "r");  *stdin = *fi;  setvbuf(stdin, NULL, _IONBF, 0);
+	FILE *fo = _fdopen(_open_osfhandle((long)output, _O_TEXT), "w"); *stdout = *fo; setvbuf(stdout, NULL, _IONBF, 0);
+	FILE *fe = _fdopen(_open_osfhandle((long)eoutput, _O_TEXT), "w"); *stderr = *fe; setvbuf(stderr, NULL, _IONBF, 0);
+
+	::SetConsoleMode(input, ENABLE_ECHO_INPUT | ENABLE_PROCESSED_INPUT
+		| ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT | ENABLE_PROCESSED_OUTPUT |
+		ENABLE_WRAP_AT_EOL_OUTPUT | ENABLE_LINE_INPUT | 0x0004 | 0x0040);
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN);
+}
+
+bool MainApp::OnInit()
+{
+	redirectSTD2console();
+    UINT openFilesMessage = RegisterWindowMessage("Decoda_OpenFiles");
+
+    // Check to see if another instances is running.
+
+    if (!wxApp::OnInit())
+    {
+        return false;
+    }
+
+    wxImage::AddHandler(new wxXPMHandler);
+    wxImage::AddHandler(new wxPNGHandler);
+
+    MainFrame* frame = new MainFrame("Script Debugger", openFilesMessage, wxDefaultPosition, wxSize(1024,768));
+
+    HWND hWndPrev = m_singleInstance.Connect(reinterpret_cast<HWND>(frame->GetHandle()), "Decoda");
+
+    // If we're loading files from the command line (but not a project) file,
+    // then don't create a new instance. This happens when the user double clicks
+    // in Explorer.
+    if (hWndPrev != NULL && m_loadProjectName.IsEmpty() && m_debugExe.IsEmpty() && m_loadFileNames.Count() > 0)
+    {
+
+        // Send the command line to the other window.
+
+        wxString fileNames;
+
+        for (unsigned int i = 0; i < m_loadFileNames.Count(); ++i)
+        {
+            fileNames += m_loadFileNames[i] + ";";
+        }
+
+        m_singleInstance.SetCommand(fileNames);
+        PostMessage(hWndPrev, openFilesMessage, 0, 0);
+
+        // Show the other window.
+        SetForegroundWindow(hWndPrev);
+        
+        delete frame;
+        return false;
+
+    }
+
+    frame->ApplyWindowPlacement();
+
+    // If a project was specified, open it.
+    if (!m_loadProjectName.IsEmpty())
+    {
+        frame->OpenProject(m_loadProjectName);
+    }
+
+    // Load any files that were specified on the command line.
+    frame->OpenDocuments(m_loadFileNames);
+
+    // Tell the UI to load the last project if we haven't instructed.
+    // it to load anything else.
+
+    if (m_loadProjectName.IsEmpty() && m_loadFileNames.IsEmpty())
+    {
+        frame->AutoOpenLastProject();        
+    }
+
+    m_loadProjectName.Clear();
+    m_loadFileNames.Clear();
+
+    frame->CheckForUpdate();
+
+    if (!m_debugExe.IsEmpty())
+    {
+        frame->DebugExe(m_debugExe);
+    }
+
+    return true;
+
+}
+
+void MainApp::OnInitCmdLine(wxCmdLineParser& parser)
+{
+
+    // Command line options:
+    //  [project file] [files...] [/debugexe imagename]
+
+    const wxCmdLineEntryDesc cmdLineDesc[] =
+        {
+            { wxCMD_LINE_PARAM,  NULL,          NULL,       "input file", wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_MULTIPLE | wxCMD_LINE_PARAM_OPTIONAL },
+#ifndef DEDICATED_PRODUCT_VERSION
+            { wxCMD_LINE_OPTION, "debugexe",    "debugexe", "executable", wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL },
+#endif
+            { wxCMD_LINE_NONE }
+        };
+    
+    parser.SetDesc(cmdLineDesc);
+
+}
+
+bool MainApp::OnCmdLineParsed(wxCmdLineParser& parser)
+{
+ 
+    for (unsigned int i = 0; i < parser.GetParamCount(); ++i)
+    {
+
+        wxFileName fileName = parser.GetParam(i);
+
+        // File names sometimes come in in the short form from Windows, so convert
+        // to the long form.
+
+        if (fileName.GetExt().Lower() == "deproj")
+        {
+            // This is a project file, so save the name separately.
+            wxFileName fileName = parser.GetParam(i);
+            m_loadProjectName = fileName.GetLongPath();
+        }
+        else
+        {
+            wxFileName fileName = parser.GetParam(i);
+            m_loadFileNames.Add(fileName.GetLongPath());
+        }
+
+    }
+    
+#ifndef DEDICATED_PRODUCT_VERSION
+    parser.Found("debugexe", &m_debugExe);
+#endif
+ 
+    return true;
+
+}
+
+const char* MainApp::GetExeFileName() const
+{
+    return argv[0];
+}
+
+wxString MainApp::GetInstanceCommandLine() const
+{
+    return m_singleInstance.GetCommand();
+}
